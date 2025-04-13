40529cb2e3d6d20b2aa34d49876b75d5ea84accc
Aakashk990 <aakashk990@gmail.com>
2025-04-13 23:59:34 +0530

    updated

--- Practice/Day_8.py
+++ Practice/Day_8.py
@@ -117,6 +117,11 @@ import re
 str = "Emma is a data scientist who knows Python. Emma works at google."
 print(str.rfind("Emma"))
 
+str1 = "Emma is a data scientist who knows Python. Emma works at google."
+print("Original String is:", str1)
+
+index = str1.rfind("Emma")
+print("Last occurrence of Emma starts at index:", index)
 """
 
  
