8c80b6f2bdf2d43abcaaf23836d8ae74dabd74cd
Aakashk990 <aakashk990@gmail.com>
2025-04-15 23:51:51 +0530

    Update Day_9.py

--- Practice/Day_9.py
+++ Practice/Day_9.py
@@ -22,4 +22,18 @@ print([x + y for x,y in zip(list1,list2)])
 """
 
 
+"""  Turn every item of a list into its square
+Given a list of numbers. write a program to turn every item of a list into its square.
+
+Given:
+
 numbers = [1, 2, 3, 4, 5, 6, 7]
+Expected output:
+
+[1, 4, 9, 16, 25, 36, 49]
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+l=[x**2 for x in numbers]
+print(l)
+
+"""
