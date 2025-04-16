56f1ca0b08d67c0ceeb1bd2573c8e2eaad0650c5
Aakashk990 <aakashk990@gmail.com>
2025-04-17 00:08:40 +0530

    updations

---
+++ Practice/Day_10/dict.py
@@ -0,0 +1,288 @@
+""" Convert two lists into a dictionary
+given:
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+Expected output:
+
+{'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+
+
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+l1=dict(zip(keys,values))
+print(l1)
+"""
+
+
+""" Merge two Python dictionaries into one
+
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+Expected output:
+{'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+solution 1 -(Python 3.5+)
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+dict3={**dict1,**dict2}
+print(dict3)
+
+solution 2- 
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+dict3 = dict1.copy()
+dict3.update(dict2)
+print(dict3)
+
+"""
+
+
+""" Print the value of key ‘history’ from the below dict
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+Expected output:
+
+80
+
+
+sampleDict = {"class": {"student": {"name": "Mike","marks": {"physics": 70,"history": 80}}}}
+print(sampleDict['class']['student']['marks']['history'])
+
+
+"""
+
+"""Initialize dictionary with default values
+In Python, we can initialize the keys with the same values.
+
+Given:
+
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
+Expected output:
+
+{'Kelly': {'designation': 'Developer', 'salary': 8000}, 'Emma': {'designation': 'Developer', 'salary': 8000}}
+
+
+
+employees = ['Kelly', 'Emma']
+defaults = {"designation": 'Developer', "salary": 8000}
+
+res = dict.fromkeys(employees, defaults)
+print(res)
+
+# Individual data
+print(res["Kelly"])
+"""
+
+""" Create a dictionary by extracting the keys from a given dictionary
+Write a Python program to create a new dictionary by extracting the mentioned keys from the below dictionary.
+
+Given dictionary:
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Keys to extract
+keys = ["name", "salary"]
+
+Expected output:
+{'name': 'Kelly', 'salary': 8000}
+
+
+solution 1-
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Keys to extract
+keys = ["name", "salary"]
+
+new_dict=dict()
+for x in keys:
+    new_dict.update({x:sample_dict[x]})
+
+
+print(new_dict)
+
+solution 2 - Dictionary Comprehension
+
+sampleDict = { 
+  "name": "Kelly",
+  "age":25, 
+  "salary": 8000, 
+  "city": "New york" }
+
+keys = ["name", "salary"]
+
+newDict = {k: sampleDict[k] for k in keys}
+print(newDict)
+
+"""
+
+
+""" Delete a list of keys from a dictionary
+Given:
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+# Keys to remove
+keys = ["name", "salary"]
+
+Expected output:
+{'city': 'New york', 'age': 25}
+
+
+solution 1- 
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+# Keys to remove
+keys = ["name", "salary"]
+
+for x in keys:
+    sample_dict.pop(x)
+
+print(sample_dict)
+
+
+Solution 2: Dictionary Comprehension
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+# Keys to remove
+keys = ["name", "salary"]
+
+sample_dict = {k: sample_dict[k] for k in sample_dict.keys() - keys}
+print(sample_dict)
+
+"""
+
+""" Check if a value exists in a dictionary
+We know how to check if the key exists in a dictionary. Sometimes it is required to check if the given value is present.
+
+Write a Python program to check if value 200 exists in the following dictionary.
+
+Given:
+
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+Expected output:
+
+200 present in a dict
+
+
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+for x in sample_dict.values():
+    if x==200:
+        print('present')
+
+"""
+
+""" Rename key of a dictionary
+Write a program to rename a key city to a location in the following dictionary.
+
+Given:
+
+sample_dict = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+}
+Expected output:
+
+{'name': 'Kelly', 'age': 25, 'salary': 8000, 'location': 'New york'}
+
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+sample_dict['location'] = sample_dict.pop('city')
+print(sample_dict)
+
+"""
+
+
+"""  Get the key of a min & max value from the following dictionary
+sample_dict = {
+  'Physics': 82,
+  'Math': 65,
+  'history': 75
+}
+
+
+sample_dict = {
+  'Physics': 82,
+  'Math': 65,
+  'history': 75
+}
+print(max(sample_dict))
+print(min(sample_dict))
+
+"""
+
+""" Change value of a key in a nested dictionary
+Write a Python program to change Brad’s salary to 8500 in the following dictionary.
+
+Given:
+sample_dict = {
+    'emp1': {'name': 'Jhon', 'salary': 7500},
+    'emp2': {'name': 'Emma', 'salary': 8000},
+    'emp3': {'name': 'Brad', 'salary': 500}
+}
+
+Expected output:
+{
+   'emp1': {'name': 'Jhon', 'salary': 7500},
+   'emp2': {'name': 'Emma', 'salary': 8000},
+   'emp3': {'name': 'Brad', 'salary': 8500}
+}
+
+
+sample_dict = {
+    'emp1': {'name': 'Jhon', 'salary': 7500},
+    'emp2': {'name': 'Emma', 'salary': 8000},
+    'emp3': {'name': 'Brad', 'salary': 500}
+}
+
+sample_dict['emp3']['salary']=8500
+print(sample_dict['emp3']['salary'])
+
+
+"""
+
