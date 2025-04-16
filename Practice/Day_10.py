ff4f61b47856e8d5fa6bf31e94a7f5d8186aa4a5
Aakashk990 <aakashk990@gmail.com>
2025-04-17 00:18:01 +0530

    Update Day_10.py

--- Practice/Day_10.py
+++ Practice/Day_10.py
@@ -285,6 +285,13 @@ print(sample_dict['emp3']['salary'])
 
 
 """
+
+
+
+#################### TUPLES  ###################
+
+
+
 """ Reverse the tuple
 
 tuple1 = (10, 20, 30, 40, 50)
@@ -306,6 +313,32 @@ print(tuple1[1][1])
 """
 
 
+""" Unpack the tuple into 4 variables
+Write a program to unpack the following tuple into four variables and display each variable.
+
+Given:
+
+tuple1 = (10, 20, 30, 40)
+Expected output:
+
+tuple1 = (10, 20, 30, 40)
+# Your code
+print(a) # should print 10
+print(b) # should print 20
+print(c) # should print 30
+print(d) # should print 40
+
+
+tuple1 = (10, 20, 30, 40)
+
+# unpack tuple into 4 variables
+a, b, c, d = tuple1
+print(a)
+print(b)
+print(c)
+print(d)
+
+"""
 
 
 
