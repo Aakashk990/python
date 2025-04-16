67085233681465074705f110f70360b8dc781012
Aakashk990 <aakashk990@gmail.com>
2025-04-17 00:14:30 +0530

    Update Day_10.py

--- Practice/Day_10.py
+++ Practice/Day_10.py
@@ -285,4 +285,36 @@ print(sample_dict['emp3']['salary'])
 
 
 """
+""" Reverse the tuple
+
+tuple1 = (10, 20, 30, 40, 50)
+print(tuple1[::-1])
+
+"""
+
+""" Access value 20 from the tuple
+The given tuple is a nested tuple. write a Python program to print the value 20.
+
+Given:
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+
+Expected output:
+20
+
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+print(tuple1[1][1])
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
 
