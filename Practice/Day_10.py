baa5dd560ec2b11eceb5162503899c3d7779e086
Aakashk990 <aakashk990@gmail.com>
2025-04-17 00:03:35 +0530

    Update Day_10.py

--- Practice/Day_10.py
+++ Practice/Day_10.py
@@ -86,12 +86,172 @@ print(res)
 print(res["Kelly"])
 """
 
+""" Create a dictionary by extracting the keys from a given dictionary
+Write a Python program to create a new dictionary by extracting the mentioned keys from the below dictionary.
+
+Given dictionary:
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Keys to extract
+keys = ["name", "salary"]
+
+Expected output:
+{'name': 'Kelly', 'salary': 8000}
 
 
+solution 1-
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Keys to extract
+keys = ["name", "salary"]
+
+new_dict=dict()
+for x in keys:
+    new_dict.update({x:sample_dict[x]})
 
 
+print(new_dict)
+
+solution 2 - Dictionary Comprehension
+
+sampleDict = { 
+  "name": "Kelly",
+  "age":25, 
+  "salary": 8000, 
+  "city": "New york" }
+
+keys = ["name", "salary"]
+
+newDict = {k: sampleDict[k] for k in keys}
+print(newDict)
+
+"""
 
 
+""" Delete a list of keys from a dictionary
+Given:
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+# Keys to remove
+keys = ["name", "salary"]
+
+Expected output:
+{'city': 'New york', 'age': 25}
 
 
+solution 1- 
 
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+# Keys to remove
+keys = ["name", "salary"]
+
+for x in keys:
+    sample_dict.pop(x)
+
+print(sample_dict)
+
+
+Solution 2: Dictionary Comprehension
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+# Keys to remove
+keys = ["name", "salary"]
+
+sample_dict = {k: sample_dict[k] for k in sample_dict.keys() - keys}
+print(sample_dict)
+
+"""
+
+""" Check if a value exists in a dictionary
+We know how to check if the key exists in a dictionary. Sometimes it is required to check if the given value is present.
+
+Write a Python program to check if value 200 exists in the following dictionary.
+
+Given:
+
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+Expected output:
+
+200 present in a dict
+
+
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+for x in sample_dict.values():
+    if x==200:
+        print('present')
+
+"""
+
+""" Rename key of a dictionary
+Write a program to rename a key city to a location in the following dictionary.
+
+Given:
+
+sample_dict = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+}
+Expected output:
+
+{'name': 'Kelly', 'age': 25, 'salary': 8000, 'location': 'New york'}
+
+
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+
+sample_dict['location'] = sample_dict.pop('city')
+print(sample_dict)
+
+"""
+
+
+"""  Get the key of a min & max value from the following dictionary
+sample_dict = {
+  'Physics': 82,
+  'Math': 65,
+  'history': 75
+}
+
+
+sample_dict = {
+  'Physics': 82,
+  'Math': 65,
+  'history': 75
+}
+print(max(sample_dict))
+print(min(sample_dict))
+
+"""
