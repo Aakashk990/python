c39870a018ac70431126aeee21c93e775c471624
Aakashk990 <aakashk990@gmail.com>
2025-04-21 00:34:02 +0530

    Update Day_12.py

--- Practice/Day_12.py
+++ Practice/Day_12.py
@@ -15,3 +15,27 @@ t1=set(zip(first_list,second_list))
 print(t1)
 
 """
+
+""" Find the intersection (common) of two sets and remove those elements from the first set
+
+Given:
+first_set = {23, 42, 65, 57, 78, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+Expected Output:
+Intersection is  {57, 83, 29}
+First Set after removing common element  {65, 42, 78, 23}
+
+first_set = {23, 42, 65, 57, 78, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+#t={first_set.remove(a) for a in first_set for b in second_set if a==b}
+c=first_set.intersection(second_set)
+print(c)
+
+for i in c:
+    first_set.remove(i)
+
+print(first_set)
+
+"""
