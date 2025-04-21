0dbf573eb843daa03b617490fb7cc535ecbb9d17
Aakashk990 <aakashk990@gmail.com>
2025-04-22 00:28:31 +0530

    Update Day_13.py

--- Practice/Day_13.py
+++ Practice/Day_13.py
@@ -39,13 +39,57 @@ print("Second Set ", second_set)
 
 """
 
+""" Iterate a given list and check if a given element exists as a key’s value in a dictionary. If not, delete it from the list
+
+Given:
+roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+sample_dict = {'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97}
+
+Expected Outcome:
+After removing unwanted elements from list [47, 69, 76, 97]
+
+roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+sample_dict = {'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97}
+print(sample_dict.values())
+new_l=[]
+for x in roll_number:
+    if x in sample_dict.values():
+        new_l.append(x)
+
+roll_number[:]=[ x for x in roll_number if x in sample_dict.values() ]
+print(new_l)
+print(roll_number)
+"""
 
 
 
+""" Get all values from the dictionary and add them to a list but don’t add duplicates
+
+Given:
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
+
+Expected Outcome:
+[47, 52, 44, 53, 54]
+
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
+l=[]
+l[:]=speed.values()
+new_l=list(set(l))
+print(new_l)
 
 
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53,
+         'july': 54, 'Aug': 44, 'Sept': 54}
 
+print("Dictionary's values - ", speed.values())
 
+speed_list = list()
 
-
+# iterate dict values
+for val in speed.values():
+    # check if value not present in a list
+    if val not in speed_list:
+        speed_list.append(val)
+print("unique list", speed_list)
+"""
 
