179b53ea45e72f7ac0f61c69422e273d4f9708c4
Aakashk990 <aakashk990@gmail.com>
2025-04-23 00:43:41 +0530

    Update Day_14.py

--- Practice/Day_14.py
+++ Practice/Day_14.py
@@ -32,3 +32,35 @@ def outer(a,b):
 print(outer(2,3))
 
 """
+
+""" Create a recursive function
+Write a program to create a recursive function to calculate the sum of numbers from 0 to 10.
+
+A recursive function is a function that calls itself again and again.
+
+Expected Output:
+55
+
+def s(n):
+    if n:
+        return(n+s(n-1))
+    else:
+        return 0
+
+print(s(10))
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
