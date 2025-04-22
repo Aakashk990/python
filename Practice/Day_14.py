9e14e56b04a8fb2f39e1fa730954ddec92d3e7fd
Aakashk990 <aakashk990@gmail.com>
2025-04-23 00:29:26 +0530

    Update Day_14.py

--- Practice/Day_14.py
+++ Practice/Day_14.py
@@ -1,7 +1,18 @@
-work_hr=[('A',100),('B',4000),('C',800)]
+""" Find the highest worked emp
+
+work_hr=[('A',100),('B',4000),('C',800),('D',8000)]
 
 def check(work_hr):
-    print(work_hr)
-
+    n=0
+    m=''
+    for x,y in work_hr:
+        if y>n:
+            n=y
+            m=x
+    
+    return(m,n)
+    
 r=check(work_hr)
 print(r)
+
+"""
